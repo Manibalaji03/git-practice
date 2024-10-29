2504dc0eeedb8591d42f8ff4c619f8241009d7ed
karthick-10 <karthick.r@brimmatech.com>
2024-10-29 15:25:46 +0530

    added function

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.print("hello");
+        System.out.println("hello");
+        add();
     }
-    public static void add(String[] args)
+    public static void add()
     {
         System.out.print("Num1+Num2");
     }
