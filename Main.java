8f6694cb030f8c819e66961012253ba04bae7305
Dharmar-brimma <dharmar@brimmatech.com>
2024-10-29 15:20:32 +0530

    add some line

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.print("hello");
-
+        System.out.print("Dharmar");
     }
-    public static int substract(int num1,int num2){
-        return num1-num2;
-    }
+//    public static int substract(int num1,int num2){
+//        return num1-num2;
+//    }
 }
